b746b8c5af844b643dab0db8466bea9d0ee3e8ec
Gauravsahu889 <138996708+Gauravsahu889@users.noreply.github.com>
2026-08-16 17:46:43 +0530

    Time: 0 ms (100%), Space: 131.5 MB (6.09%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0, 0, 0};
+        for (int s : stones) {
+            cnt[s % 3]++;
+        }
+
+        if (cnt[0] % 2 == 0) {
+            // Even number of "junk" stones (remainder 0)
+            return cnt[1] >= 1 && cnt[2] >= 1;
+        } else {
+            // Odd number of "junk" stones
+            return abs(cnt[1] - cnt[2]) >= 3;
+        }
+    }
+};
